648cc32ff8ee485bd62476560665783f34e45edf
G4II0 <smugallo@gmail.com>
2023-05-13 01:17:44 +0200

    cd

--- View.java
+++ View.java
@@ -7,48 +7,21 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
-public class klotski
+public class View
 {
     Board board;
 
-    Pieces[] p = board.getPieces;
+    Piece[] p = board.getPieces();
+
+    for(
+    int i = 0;i<p.length;++i)
+    {
+        int[] currentDims;
+        //restituisce: x, y, altezza e larghezza
+        currentDims = p[i].getDims();
+    }
 
 }
-
-public class View extends Application {
-    private Rectangle square;
-
-    @Override
-    public void start(Stage primaryStage) {
-        square = new Rectangle(100, 100, Color.RED);
-        square.setStroke(Color.BLACK);
-        square.setStrokeWidth(2);
-
-        StackPane root = new StackPane();
-        root.getChildren().add(square);
-        root.setPadding(new Insets(10));
-        root.setAlignment(Pos.CENTER);
-
-        Scene scene = new Scene(root, 300, 250);
-        scene.widthProperty().addListener((obs, oldVal, newVal) -> resizeSquare(newVal.doubleValue()));
-        scene.heightProperty().addListener((obs, oldVal, newVal) -> resizeSquare(newVal.doubleValue()));
-
-        primaryStage.setTitle("Resizable Square");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
-    private void resizeSquare(double size) {
-        square.setWidth(size / 2);
-        square.setHeight(size / 2);
-    }
-
-    
-    public static void main(String[] args) {
-        launch(args);
-    }
-}*/
-
 public class View extends Application {
 private Rectangle square;
 
