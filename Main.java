d4243fb440ab569bc1b357d5280f37c48ea4d470
Neilkris01 <alvisestella@gmail.com>
2023-05-13 16:18:18 +0200

    cvb

--- Main.java
+++ Main.java
@@ -1,32 +1,13 @@
 import javafx.application.Application;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    Board board = new Board();
-    Piece[] p = board.getPieces();
-
-    private int squareSize = 50; // dimensione del lato dei quadrati
-
     @Override
-    public void start(Stage primaryStage) throws Exception {
-
-        // Create rectangles for each square in the grid
-        BoardView boardView = new BoardView();
-        Rectangle[][] squares = new Rectangle[5][4];
-        boardView.createBoard(squares, squareSize);
-
-        // Set up the initial state of the game
-        PiecesView piecesView = new PiecesView();
-        piecesView.printPiece(p, squares);
-
-        // Create a layout grid for the squares
-        // Add the squares to the grid
-        // Create the root group and add the grid to it
-        // Create the scene and set its dimensions
-        // Set the scene on the stage and display the stage
-        boardView.gridLayout(squares, primaryStage);
+    public void start(Stage primaryStage) {
+        Board board = new Board();
+        ViewPrint view = new ViewPrint(primaryStage);
+        view.print(primaryStage);
     }
 
     public static void main(String[] args) {
