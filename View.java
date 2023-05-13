d6c3e1d07044860091b9f24f8f9033ec60dfb104
G4II0 <smugallo@gmail.com>
2023-05-13 02:20:59 +0200

    fgh

--- View.java
+++ View.java
@@ -30,37 +30,4 @@ public class View /*extends Application*/
             int h2 = pDim2[2];
             int w2 = pDim2[3];
     }
-/*
-    private Rectangle square;
-
-    @Override
-        public void start(Stage primaryStage) throws Exception {
-            
-            // Creazione di un rettangolo per rappresentare un pezzo da 2x1
-            Rectangle piece21 = new Rectangle(50, 100, 100, 50);
-            piece21.setFill(Color.RED);
-            
-            // Creazione di un rettangolo per rappresentare un pezzo da 1x1
-            Rectangle piece11 = new Rectangle(200, 150, 50, 50);
-            piece11.setFill(Color.BLUE);
-
-            //Creazione griglia 5x4
-            square = new Rectangle(100, 100, Color.RED);
-            square.setStroke(Color.BLACK);
-            square.setStrokeWidth(2);
-
-            StackPane root = new StackPane();
-            root.getChildren().add(square);
-            root.setPadding(new Insets(10));
-            root.setAlignment(Pos.CENTER);
-
-            Scene scene = new Scene(root, 300, 250);
-            scene.widthProperty().addListener((obs, oldVal, newVal) -> resizeSquare(newVal.doubleValue()));
-            scene.heightProperty().addListener((obs, oldVal, newVal) -> resizeSquare(newVal.doubleValue()));
-
-            primaryStage.setTitle("Resizable Square");
-            primaryStage.setScene(scene);
-            primaryStage.show();
-            
-    }*/
 }
