568f6834d707709590f185ab2f62fd40cdaf09b2
Samuele Gallo <smugallo@gmail.com>
2023-06-09 12:14:32 +0200

    marge fix

--- KlotskiSolver.java
+++ KlotskiSolver.java
@@ -9,7 +9,44 @@ import java.util.Set;
 
 
 
-public class KlotskiSolver {
+public class KlotskiSolver
+{
+
+	String[][] grid = new String[4][5]; // board girata di 90gradi
+
+	public void boardToGrid(Piece[] pieces) {
+		for (int i = 0; i < 10; i++) {
+			int x1 = pieces[i].getDims()[0];
+			int y1 = pieces[i].getDims()[1];
+			if (pieces[i].getDims()[2] == 2 && pieces[i].getDims()[3] == 2) // pezzo 2x2
+			{
+				int x2 = x1;
+				int y2 = y1++;
+				int x3 = x1++;
+				int y3 = y1;
+				int x4 = x1++;
+				int y4 = y1++;
+				grid[3 - y1][x1] = pieces[i].getName();
+				grid[3 - y2][x2] = pieces[i].getName();
+				grid[3 - y3][x3] = pieces[i].getName();
+				grid[3 - y4][x4] = pieces[i].getName();
+			} else if (pieces[i].getDims()[2] == 2 || pieces[i].getDims()[3] == 2) // pezzi 1x2 o 2x1
+			{
+				int x2 = 0, y2 = 0;
+				for (int j = 0; j < (pieces[i].getDims()[2]); j++) {
+					x2 = x1 + j;
+					for (int j1 = 0; j1 < (pieces[i].getDims()[3]); j1++) {
+						y2 = y1 + j1;
+
+					}
+				}
+				grid[3 - y1][x1] = pieces[i].getName();
+				grid[3 - y2][x2] = pieces[i].getName();
+			} else if (pieces[i].getDims()[2] == 1 && pieces[i].getDims()[3] == 1) {
+				grid[3 - y1][x1] = pieces[i].getName();
+			}
+		}
+	}
 	
 	KlotskiPuzzle puzzle;
 	Map<String, String> tree = new Hashtable<String, String>();
@@ -143,6 +180,69 @@ public class KlotskiSolver {
 	{
 		KlotskiPuzzle p = new KlotskiPuzzle(gridCode);
 		return p.isSolved();
+	}
+
+	public void puzzleTranslateOutput(String kSolverOutput, Piece[] pieces)
+	{
+		char[] caratteri = kSolverOutput.toCharArray();
+		int aC = 0, bC = 0, cC = 0, dC = 0, eC = 0, jC = 0, countx = 3, county = 0, j = 0;
+
+		for (county = 0; county < 5; county++) {
+			for (countx = 3; countx >= 0; countx--) {
+				if (caratteri[j] == 'A' && aC == 0) {
+					pieces[0].setPos(countx, county);
+					j++;
+					aC++;
+				} else if (caratteri[j] == 'A' && aC == 1) {
+					j++;
+				} else if (caratteri[j] == 'B' && bC == 0) {
+					pieces[1].setPos(countx, county);
+					j++;
+					bC++;
+				} else if (caratteri[j] == 'B' && bC == 1) {
+					j++;
+				} else if (caratteri[j] == 'C' && cC == 0) {
+
+					pieces[2].setPos(countx, county);
+					j++;
+					cC++;
+				} else if (caratteri[j] == 'C' && cC == 1) {
+					j++;
+				} else if (caratteri[j] == 'D' && dC == 0) {
+					pieces[3].setPos(countx, county);
+					j++;
+					dC++;
+				} else if (caratteri[j] == 'D' && dC == 1) {
+					j++;
+				} else if (caratteri[j] == 'E' && eC == 1) {
+					pieces[4].setPos(countx, county);
+					j++;
+				} else if (caratteri[j] == 'E' && eC == 0) {
+					eC++;
+					j++;
+				} else if (caratteri[j] == 'F') {
+					pieces[5].setPos(countx, county);
+					j++;
+				} else if (caratteri[j] == 'G') {
+					pieces[6].setPos(countx, county);
+					j++;
+				} else if (caratteri[j] == 'H') {
+					pieces[7].setPos(countx, county);
+					j++;
+				} else if (caratteri[j] == 'I') {
+					pieces[8].setPos(countx, county);
+					j++;
+				} else if (caratteri[j] == 'J' && jC == 1) {
+					pieces[9].setPos(countx, county);
+					jC++;
+				} else if (caratteri[j] == 'J' && jC == 0) {
+					j++;
+					jC++;
+				} else if (caratteri[j] == '0') {
+					j++;
+				}
+			}
+		}
 	}
 	
 	/*private void printSolution(String solution){
